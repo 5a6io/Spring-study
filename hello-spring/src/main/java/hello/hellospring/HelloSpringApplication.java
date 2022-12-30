c87fdec873993fab0000128d9464d49ba828d827
yunhyemi <yhm54321@naver.com>
2022-12-30 17:51:55 +0900

    spring

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -3,7 +3,9 @@ package hello.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 자체적인 톰켓이 내장되어 있음.
+
+/*setting or preferences -> gradle -> build and run 부분 gradle에서 intellij로 변경한 후 사용하면 빠름*/
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
