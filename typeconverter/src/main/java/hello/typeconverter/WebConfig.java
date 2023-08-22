1562b1f19654a46ef27d283934b764b76626eac2
HYEMI YUN <yhm54321@naver.com>
2023-08-22 13:36:43 +0900

    spring mvc part2 section10

---
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -0,0 +1,26 @@
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
+import hello.typeconverter.type.IpPortToStringConverter;
+import hello.typeconverter.type.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        //주석 처리 우선 순위
+        //registry.addConverter(new StringToIntegerConverter());
+        //registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
+    }
+}
